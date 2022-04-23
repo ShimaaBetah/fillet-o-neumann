ef60485e1e0eb1f5feb190dd6594c4ff83d32b57
AhmedNasserG <ahmednasser217217@gmail.com>
2022-04-24 00:20:15 +0200

    Refactor Decoder utility
    Co-authored-by: Abdulaziz Hassan <abdulazizhza@gmail.com>

---
+++ src/utils/Decoder.java
@@ -0,0 +1,23 @@
+package utils;
+
+public class Decoder {
+    public static int binaryToInt(String binaryString) {
+        return Integer.parseInt(binaryString, 2);
+    }
+
+    public static String intToBinary(int integerValue) {
+        String binaryString = Integer.toBinaryString(integerValue);
+        StringBuilder sb = new StringBuilder(32);
+        for (int i = binaryString.length(); i < 32; i++) {
+            sb.append("0");
+        }
+        sb.append(binaryString);
+        return sb.toString();
+    }
+
+    public static int getIntValueOfBinarySegment(int integerValue, int start, int end) {
+        String binaryString = intToBinary(integerValue);
+        String binaryStringSegment = binaryString.substring(start, end + 1);
+        return binaryToInt(binaryStringSegment);
+    }
+}
