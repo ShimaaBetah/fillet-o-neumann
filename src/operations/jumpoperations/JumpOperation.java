1784e893480d91add1a2915f9028b08ec763159a
AhmedNasserG <ahmednasser217217@gmail.com>
2022-04-24 12:56:36 +0200

    Add Jump instruction

---
+++ src/operations/jumpoperations/JumpOperation.java
@@ -0,0 +1,22 @@
+package operations.jumpoperations;
+
+import operations.Operation;
+
+public abstract class JumpOperation implements Operation {
+    private final int opcode;
+    private final int address;
+
+    public JumpOperation(int opcode, int address) {
+        this.opcode = opcode;
+        this.address = address;
+    }
+
+    public int getOpcode() {
+        return opcode;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+}
