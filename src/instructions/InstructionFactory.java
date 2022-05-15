3b426a194322b6ab40de8d7f6633052f554b510e
AhmedNasserG <ahmednasser217217@gmail.com>
2022-05-15 19:58:16 +0200

    Refactor InstructionFactory

--- src/instructions/InstructionFactory.java
+++ src/instructions/InstructionFactory.java
@@ -7,26 +7,26 @@ import java.util.HashMap;
 public class InstructionFactory {
     private static final int OPCODE_RANGE_START = 0;
     private static final int OPCODE_RANGE_END = 3;
-    private static final HashMap<Integer, InstructionType> opcodeMap = new HashMap<>();
+    private static final HashMap<Integer, InstructionType> opcodeToInstruction = new HashMap<>();
 
     static {
-        opcodeMap.put(0, InstructionType.RType);
-        opcodeMap.put(1, InstructionType.RType);
-        opcodeMap.put(2, InstructionType.RType);
-        opcodeMap.put(3, InstructionType.IType);
-        opcodeMap.put(4, InstructionType.IType);
-        opcodeMap.put(5, InstructionType.RType);
-        opcodeMap.put(6, InstructionType.IType);
-        opcodeMap.put(7, InstructionType.JType);
-        opcodeMap.put(8, InstructionType.RType);
-        opcodeMap.put(9, InstructionType.RType);
-        opcodeMap.put(10, InstructionType.IType);
-        opcodeMap.put(11, InstructionType.IType);
+        opcodeToInstruction.put(0, InstructionType.RType);
+        opcodeToInstruction.put(1, InstructionType.RType);
+        opcodeToInstruction.put(2, InstructionType.RType);
+        opcodeToInstruction.put(3, InstructionType.IType);
+        opcodeToInstruction.put(4, InstructionType.IType);
+        opcodeToInstruction.put(5, InstructionType.RType);
+        opcodeToInstruction.put(6, InstructionType.IType);
+        opcodeToInstruction.put(7, InstructionType.JType);
+        opcodeToInstruction.put(8, InstructionType.RType);
+        opcodeToInstruction.put(9, InstructionType.RType);
+        opcodeToInstruction.put(10, InstructionType.IType);
+        opcodeToInstruction.put(11, InstructionType.IType);
     }
 
     public Instruction create(int binaryInstruction) {
         int opcode = getOpcode(binaryInstruction);
-        InstructionType instructionType = opcodeMap.get(opcode);
+        InstructionType instructionType = opcodeToInstruction.get(opcode);
         Instruction instruction = null;
         if (instructionType == InstructionType.RType) {
             instruction = new RegisterInstruction(binaryInstruction);
