2315a6c1a197ab26bdb44751f2892128dc3392f4
AhmedNasserG <ahmednasser217217@gmail.com>
2022-05-15 20:05:34 +0200

    Add JumpOperationFactory

--- src/instructions/JumpInstruction.java
+++ src/instructions/JumpInstruction.java
@@ -1,36 +1,22 @@
 package instructions;
 
 import operations.Operation;
+import operations.jumpoperations.JumpOperationFactory;
 import utils.Decoder;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class JumpInstruction extends Instruction {
-    private static final int OPCODE_RANGE_START = 0;
-    private static final int OPCODE_RANGE_END = 3;
-    private static final int ADDRESS_RANGE_START = 4;
-    private static final int ADDRESS_RANGE_END = 31;
 
     public JumpInstruction(int binaryInstruction) {
         super(binaryInstruction);
+        setOperationFactory(new JumpOperationFactory());
     }
 
     @Override
     public void decode() {
-        int opcode = Decoder.getIntValueOfBinarySegment(getBinaryInstruction(), OPCODE_RANGE_START, OPCODE_RANGE_END);
-        int address = Decoder.getIntValueOfBinarySegment(getBinaryInstruction(), ADDRESS_RANGE_START, ADDRESS_RANGE_END);
-        setOperation(opcode, address);
+        Operation operation = getOperationFactory().create(getBinaryInstruction());
+        setOperation(operation);
     }
 
-    private void setOperation(int opcode, int address) {
-        Class<? extends Operation> operationClass = getOperationsMap().get(opcode);
-        Constructor<? extends Operation> operationConstructor;
-        try {
-            operationConstructor = operationClass.getConstructor(int.class, int.class);
-            Operation operation = operationConstructor.newInstance(opcode, address);
-            setOperation(operation);
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
-    }
 }
