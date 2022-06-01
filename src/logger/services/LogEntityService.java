525c7a44f403f9b7db968f56295bcbd8ea397e00
AhmedNasserG <ahmednasser217217@gmail.com>
2022-06-01 21:10:55 +0200

    Add logPipeline method to LogEntityService
    Co-authored-by: Elshimaa Betah <ShimaaBetah@users.noreply.github.com>
    Co-authored-by: Abdulaziz Hassan <abdulazizhza@gmail.com>

--- src/logger/services/LogEntityService.java
+++ src/logger/services/LogEntityService.java
@@ -1,7 +1,8 @@
 package logger.services;
 
+import instructions.Instruction;
 import memory.MainMemory;
-import memory.Registers;
+import memory.RegisterFile;
 import utils.Binary;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import exceptions.AddressOutOfRangeException;
 import exceptions.InvalidRegisterNumberException;
 
 public class LogEntityService {
-    public static String logRegisters(Registers registers, int[] registersToLog) {
+    public static String logRegisters(RegisterFile registerFile, int[] registersToLog) {
         List<String> headers = Arrays.asList("Register", "Value");
 
         List<List<String>> rows = new ArrayList<>();
@@ -20,7 +21,7 @@ public class LogEntityService {
             List<String> row = new ArrayList<>();
             row.add(String.valueOf(regIdx));
             try {
-                row.add(String.valueOf(registers.getRegister(regIdx)));
+                row.add(String.valueOf(registerFile.getRegister(regIdx)));
             } catch (InvalidRegisterNumberException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -31,26 +32,7 @@ public class LogEntityService {
         return GenerateTableService.execute(headers, rows);
     }
 
-    public static String logMainMemoryData(MainMemory memory, int startAddress, int endAddress) {
-        List<String> headers = Arrays.asList("Address", "Value");
-
-        List<List<String>> rows = new ArrayList<>();
-        for (int i = startAddress; i <= endAddress; i++) {
-            List<String> row = new ArrayList<>();
-            row.add(String.valueOf(i));
-            try {
-                row.add(String.valueOf(memory.loadData(i)));
-            } catch (AddressOutOfRangeException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            rows.add(row);
-        }
-        return GenerateTableService.execute(headers, rows);
-
-    }
-
-    public static String logMainMemoryInstructions(MainMemory memory, int startAddress, int endAddress) {
+    public static String logMainMemoryWord(MainMemory memory, int startAddress, int endAddress) {
         List<String> headers = Arrays.asList("Address", "ValueAsInt", "ValueAsBinary");
 
         List<List<String>> rows = new ArrayList<>();
@@ -58,17 +40,56 @@ public class LogEntityService {
             List<String> row = new ArrayList<>();
             row.add(String.valueOf(i));
             try {
-                int value = memory.loadInstruction(i);
+                int value = memory.loadWord(i);
                 row.add(String.valueOf(value));
                 String binary = Binary.intToBinaryString(value);
                 row.add(binary);
             } catch (AddressOutOfRangeException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             rows.add(row);
         }
 
+        return GenerateTableService.execute(headers, rows);
+    }
+
+    public static String logPipeline(int cycle, Instruction[] pipeline) {
+        //TODO: refactor this method
+        List<String> headers = Arrays.asList("Cycle", "Fetch", "Decode", "Execute", "Memory", "WriteBack");
+        final int FETCH_POSITION = 0;
+        final int DECODE_POSITION = 1;
+        final int READ_REGISTERS_POSITION = 2;
+        final int EXECUTE_POSITION1 = 3;
+        final int EXECUTE_POSITION2 = 4;
+        final int MEMORY_POSITION = 5;
+        final int WRITE_BACK_POSITION = 6;
+        final String nullInstruction = "-".repeat(13);
+        List<List<String>> rows = new ArrayList<>();
+
+        String[] row = new String[headers.size()];
+        row[0] = String.valueOf(cycle);
+        row[1] = pipeline[FETCH_POSITION] == null ? nullInstruction : "instruction " + pipeline[FETCH_POSITION].getPC();
+
+        if (pipeline[DECODE_POSITION] == null && pipeline[READ_REGISTERS_POSITION] == null) {
+            row[2] = nullInstruction;
+        } else if (pipeline[DECODE_POSITION] != null) {
+            row[2] = "instruction " + pipeline[DECODE_POSITION].getPC();
+        } else if (pipeline[READ_REGISTERS_POSITION] != null) {
+            row[2] = "instruction " + pipeline[READ_REGISTERS_POSITION].getPC();
+        }
+
+        if (pipeline[EXECUTE_POSITION1] == null && pipeline[EXECUTE_POSITION2] == null) {
+            row[3] = nullInstruction;
+        } else if (pipeline[EXECUTE_POSITION1] != null) {
+            row[3] = "instruction " + pipeline[EXECUTE_POSITION1].getPC();
+        } else if (pipeline[EXECUTE_POSITION2] != null) {
+            row[3] = "instruction " + pipeline[EXECUTE_POSITION2].getPC();
+        }
+
+        row[4] = pipeline[MEMORY_POSITION] == null ? nullInstruction : "instruction " + pipeline[MEMORY_POSITION].getPC();
+        row[5] = pipeline[WRITE_BACK_POSITION] == null ? nullInstruction : "instruction " + pipeline[WRITE_BACK_POSITION].getPC();
+
+        rows.add(Arrays.asList(row));
         return GenerateTableService.execute(headers, rows);
     }
 }
