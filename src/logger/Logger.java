0377c66353a139b0331989a5a8394d14c8824561
AhmedNasserG <ahmednasser217217@gmail.com>
2022-06-01 21:14:23 +0200

    update Logger utility
    Co-authored-by: Elshimaa Betah <ShimaaBetah@users.noreply.github.com>
    Co-authored-by: Abdulaziz Hassan <abdulazizhza@gmail.com>
    
    added log method for registers (log the whole registers file)
    added log method for main memory (log the whole memory)
    update log method for main memory (log range of the memory)

--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -1,10 +1,11 @@
 package logger;
 
+import instructions.Instruction;
 import logger.services.InitLoggerService;
 import logger.services.LogEntityService;
 import logger.services.SegmentType;
 import memory.MainMemory;
-import memory.Registers;
+import memory.RegisterFile;
 
 public class Logger {
     private static final LogSubject logSubject = InitLoggerService.execute();
@@ -20,23 +21,31 @@ public class Logger {
         log(message + "\n");
     }
 
-    public static void log(MainMemory memory, int address, SegmentType segmentType) {
-        if (segmentType == SegmentType.DATA) {
-            log(LogEntityService.logMainMemoryData(memory, address, address));
-        } else {
-            log(LogEntityService.logMainMemoryInstructions(memory, address, address));
-        }
+    public static void log(MainMemory memory, int address) {
+        log(LogEntityService.logMainMemoryWord(memory, address, address));
     }
 
-    public static void log(MainMemory memory, int startAddress, int endAddress,SegmentType segmentType) {
-        if (segmentType == SegmentType.DATA) {
-            log(LogEntityService.logMainMemoryData(memory, startAddress, endAddress));
-        } else {
-            log(LogEntityService.logMainMemoryInstructions(memory, startAddress, endAddress));
-        }
+    public static void log(MainMemory memory, int startAddress, int endAddress) {
+        log(LogEntityService.logMainMemoryWord(memory, startAddress, endAddress));
     }
 
-    public static void log(Registers registers, int[] registersToLog) {
-        log(LogEntityService.logRegisters(registers, registersToLog));
+    public static void log(MainMemory memory) {
+        logln(LogEntityService.logMainMemoryWord(memory, MainMemory.getInstance().getInstructionRangeStart(), MainMemory.getInstance().getDataRangeEnd()));
+    }
+
+    public static void log(RegisterFile registerFile, int[] registersToLog) {
+        logln(LogEntityService.logRegisters(registerFile, registersToLog));
+    }
+
+    public static void log(RegisterFile registerFile) {
+        int[] registersToLog = new int[RegisterFile.NUM_OF_REGISTERS];
+        for (int i = 0; i < RegisterFile.NUM_OF_REGISTERS; i++) {
+            registersToLog[i] = i;
+        }
+        log(registerFile, registersToLog);
+    }
+
+    public static void log(int cycle, Instruction[] pipeline) {
+        logln(LogEntityService.logPipeline(cycle, pipeline));
     }
 }
