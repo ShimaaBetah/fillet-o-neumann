bac97762e30dbcc7b0debd081e5dff2bed8fc66b
AhmedNasserG <ahmednasser217217@gmail.com>
2022-06-05 11:59:53 +0200

    [Refactor] refactored OperationFactory

--- src/main/java/fillet/operations/OperationFactory.java
+++ src/main/java/fillet/operations/OperationFactory.java
@@ -3,27 +3,39 @@ package fillet.operations;
 import java.util.HashMap;
 
 public abstract class OperationFactory {
-    private static HashMap<Integer, OperationType> opcodeToOperation = new HashMap<>();
-
-    static {
-        opcodeToOperation.put(0, OperationType.ADD);
-        opcodeToOperation.put(1, OperationType.SUBTRACT);
-        opcodeToOperation.put(2, OperationType.MULTIPLY);
-        opcodeToOperation.put(3, OperationType.MOVE_IMMEDIATE);
-        opcodeToOperation.put(4, OperationType.JUMP_IF_EQUAL);
-        opcodeToOperation.put(5, OperationType.AND);
-        opcodeToOperation.put(6, OperationType.XOR_IMMEDIATE);
-        opcodeToOperation.put(7, OperationType.JUMP);
-        opcodeToOperation.put(8, OperationType.LOGICAL_SHIFT_LEFT);
-        opcodeToOperation.put(9, OperationType.LOGICAL_SHIFT_RIGHT);
-        opcodeToOperation.put(10, OperationType.MOVE_TO_REGISTER);
-        opcodeToOperation.put(11, OperationType.MOVE_TO_MEMORY);
-        opcodeToOperation.put(15, OperationType.HALT);
-    }
 
     public abstract Operation create(int binaryInstruction, int pc);
 
     public OperationType getOperationType(int opcode) {
-        return opcodeToOperation.get(opcode);
+        switch (opcode) {
+            case 0:
+                return OperationType.ADD;
+            case 1:
+                return OperationType.SUBTRACT;
+            case 2:
+                return OperationType.MULTIPLY;
+            case 3:
+                return OperationType.MOVE_IMMEDIATE;
+            case 4:
+                return OperationType.JUMP_IF_EQUAL;
+            case 5:
+                return OperationType.AND;
+            case 6:
+                return OperationType.XOR_IMMEDIATE;
+            case 7:
+                return OperationType.JUMP;
+            case 8:
+                return OperationType.LOGICAL_SHIFT_LEFT;
+            case 9:
+                return OperationType.LOGICAL_SHIFT_RIGHT;
+            case 10:
+                return OperationType.MOVE_TO_REGISTER;
+            case 11:
+                return OperationType.MOVE_TO_MEMORY;
+            case 15:
+                return OperationType.HALT;
+            default:
+                throw new IllegalArgumentException("Unknown opcode: " + opcode);
+        }
     }
 }
